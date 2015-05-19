e09fdd6e73a875016573adce53e9384e64267674
Jimmy Griffith <rogueSkib@gmail.com>
2015-05-18 19:36:12 -0700

    updated avoider art config

--- src/defaultConfigs/avoider.js
+++ src/defaultConfigs/avoider.js
@@ -15,27 +15,27 @@ communityart.registerConfig('avoider/starscape', {
 communityart.registerConfig('avoider/rocket_ship', {
   type: 'default',
   opts: {
-    url: PREFIX + 'ship.png',
-    width: PLAYER_SIZE.w,
-    height: PLAYER_SIZE.h,
-    anchorX: PLAYER_SIZE.w / 2,
-    anchorY: PLAYER_SIZE.h / 2,
-    offsetX: PLAYER_SIZE.w / -2,
-    offsetY: PLAYER_SIZE.h / -2,
-    hitBounds: { radius: PLAYER_SIZE.w / 2 }
+    hitOpts: { radius: PLAYER_SIZE.w / 2 },
+    viewOpts: {
+      url: PREFIX + 'ship.png',
+      width: PLAYER_SIZE.w,
+      height: PLAYER_SIZE.h,
+      offsetX: PLAYER_SIZE.w / -2,
+      offsetY: PLAYER_SIZE.h / -2
+    }
   }
 });
 
 communityart.registerConfig('avoider/enemy_ship', {
   type: 'default',
   opts: {
-    url: PREFIX + 'ufo.png',
-    width: ENEMY_SIZE.w,
-    height: ENEMY_SIZE.h,
-    anchorX: ENEMY_SIZE.w / 2,
-    anchorY: ENEMY_SIZE.h / 2,
-    offsetX: ENEMY_SIZE.w / -2,
-    offsetY: ENEMY_SIZE.h / -2,
-    hitBounds: { radius: ENEMY_SIZE.w / 2 }
+    hitOpts: { radius: ENEMY_SIZE.w / 2 },
+    viewOpts: {
+      url: PREFIX + 'ufo.png',
+      width: ENEMY_SIZE.w,
+      height: ENEMY_SIZE.h,
+      offsetX: ENEMY_SIZE.w / -2,
+      offsetY: ENEMY_SIZE.h / -2
+    }
   }
 });
